095d386cae1214a564dbc623811611532b8540c9
hafsaxfarid <59932046+hafsaxfarid@users.noreply.github.com>
2022-01-25 21:46:24 -0500

    Main Menu
    Menu with buttons that will load each assignment respectively as they are added in and completed

---
+++ GAME3011_MiniGames_HF_LE/Assets/Scripts/SceneController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public void LoadAssignment1()
+    {
+        SceneManager.LoadScene("OreMining");
+    }
+
+    public void LoadAssignment2()
+    {
+        Debug.Log("A2 Coming Soon...");
+    }
+
+    public void LoadAssignment3()
+    {
+        Debug.Log("A3 Coming Soon...");
+    }
+
+    public void LoadAssignment4()
+    {
+        Debug.Log("A4 Coming Soon...");
+    }
+}
