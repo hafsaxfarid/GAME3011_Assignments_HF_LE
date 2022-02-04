f07d8bb970539a601023130556d715942913af6e
hafsaxfarid <59932046+hafsaxfarid@users.noreply.github.com>
2022-02-04 01:58:47 -0500

    Tile Click Event
    Highlights tile on mouse click

--- GAME3011_MiniGames_HF_LE/Assets/Scripts/A1/TileManager.cs
+++ GAME3011_MiniGames_HF_LE/Assets/Scripts/A1/TileManager.cs
@@ -1,17 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
 
 public class TileManager : MonoBehaviour
 {
     [SerializeField]
     public int row;
+    
+    [SerializeField]
     public int column;
+    
+    [SerializeField]
+    public GameObject highlightedColor;
 
     public void setGridCoords(int r, int c)
     {
         row = r;
         column = c;
+    }
+
+    private void OnMouseDown()
+    {
+        highlightedColor.SetActive(true);
+        Debug.Log("Row: " + row + " Column: " + column);
+    }
+
+    private void OnMouseUp()
+    {
+        highlightedColor.SetActive(false);
     }
 }
